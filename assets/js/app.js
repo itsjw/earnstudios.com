82a0362e67fee52aa3d467c1fd49eac91d59b1b6
Gareth Johns <info@garethjohnsdesign.com>
2016-09-21 00:54:47 +0100

    Initial Build

--- assets/js/app.js
+++ assets/js/app.js
@@ -85,6 +85,10 @@ wow = new WOW(
                     }
                     )
 wow.init();
-  
+
+
+var map = new google.maps.Map( document.getElementById( 'map-canvas' ), {		
+	zoom:         7,			center:       new google.maps.LatLng( originalLocation[0], originalLocation[1] ),			scrollwheel:  false		
+});
 
 
