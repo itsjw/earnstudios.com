ae8cc380eb4299368c33fdeb98460f69a0d0803d
Gareth Johns <info@garethjohnsdesign.com>
2016-09-20 13:04:25 +0100

    Initial Commit

---
+++ _projects/castrol.markdown
@@ -0,0 +1,12 @@
+---
+title: Castrol
+date: 2013-09-12 00:00:00 Z
+image: "/uploads/castrol.jpg"
+feature-image: "/uploads/castrol-feature.jpg"
+span: April 2012 – April 2013
+company: Castrol
+layout: project
+is-featured: 'true'
+is-carousel: 'true'
+carousel-text: Creators of the most shared video in the world 
+---
--- _site/assets/js/app.js
+++ _site/assets/js/app.js
@@ -15,6 +15,7 @@ if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && lo
 }
 });
 
+/*
 var $heroCarouselText;
 
 var $heroCarousel = $('.hero-carousel');
@@ -58,6 +59,19 @@ $heroCarousel.flickity({
   pageDots: true,
   arrowShape: 'M100,46.875H11.9688l17.4688-17.4688L25,25L0,50l25,25l4.4062-4.4062L11.9688,53.125H100V46.875z'
 }).on( 'select.flickity', animateText );
+*/
+
+
+$('.hero-carousel').flickity({
+  // options
+  contain: true,
+  setGallerySize: false,
+  wrapAround: true,
+  autoPlay: false,
+  prevNextButtons: false,
+  pageDots: false,
+  arrowShape: 'M100,46.875H11.9688l17.4688-17.4688L25,25L0,50l25,25l4.4062-4.4062L11.9688,53.125H100V46.875z'
+});
 
 
 
