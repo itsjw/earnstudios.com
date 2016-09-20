5a752762923e7da4b6537b2b3e7c98b79e99b20f
Gareth Johns <info@garethjohnsdesign.com>
2016-09-20 17:58:12 +0100

    Initial Commit

--- _site/assets/js/app.js
+++ _site/assets/js/app.js
@@ -69,7 +69,7 @@ $('.hero-carousel').flickity({
   wrapAround: false,
   autoPlay: true,
   prevNextButtons: false,
-  pageDots: false,
+  pageDots: true,
   arrowShape: 'M100,46.875H11.9688l17.4688-17.4688L25,25L0,50l25,25l4.4062-4.4062L11.9688,53.125H100V46.875z'
 });
 
