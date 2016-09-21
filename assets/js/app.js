04292a896cff61b8a6823bd2190f434c3aac7c00
Gareth Johns <info@garethjohnsdesign.com>
2016-09-21 18:16:15 +0100

    Updated Config and 3 other files

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,5 @@
 $(document).foundation();
 
-$('body').removeClass('fade-out');
 
 $('a[href*="#"]:not([href="#"])').click(function() {
 if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -14,53 +13,6 @@ if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && lo
   }
 }
 });
-
-/*
-var $heroCarouselText;
-
-var $heroCarousel = $('.hero-carousel');
-
-var prevIndex;
-
-function animateText(e){
-	
-	var flkty = Flickity.data( $heroCarousel[0] );
-	
-	
-	var index =  flkty.selectedIndex;
-	
-	if( prevIndex === index){
-		return;
-	}
-	prevIndex = index;
-	
-	if(!$heroCarouselText){
-		$heroCarouselText = $( ".hero-carousel-text-inside" );
-	}
-	$heroCarouselText.addClass( "animated fadeInUp" );
-	setTimeout(function(){
-		 $heroCarouselText.removeClass( "animated fadeInUp" );
-	}, 1000);
-}
-
-$(function(){
-	animateText();
-});
-
-
-$heroCarousel.flickity({
-  // options
-  contain: true,
-  draggable: false,
-  setGallerySize: false,
-  wrapAround: true,
-  autoPlay: true,
-  prevNextButtons: false,
-  pageDots: true,
-  arrowShape: 'M100,46.875H11.9688l17.4688-17.4688L25,25L0,50l25,25l4.4062-4.4062L11.9688,53.125H100V46.875z'
-}).on( 'select.flickity', animateText );
-*/
-
 
 $('.hero-carousel').flickity({
   // options
